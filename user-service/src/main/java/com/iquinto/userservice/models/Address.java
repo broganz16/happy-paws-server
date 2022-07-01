c4ad458452afb7b473a6f4cefdcd4de73c4044b4
iquinto <isaganiquinto@gmail.com>
2022-07-01 11:10:38 +0200

    Initial commit

--- user-service/src/main/java/com/iquinto/userservice/models/Address.java
+++ user-service/src/main/java/com/iquinto/userservice/models/Address.java
@@ -1,19 +1,15 @@
 package com.iquinto.userservice.models;
 
-
 import lombok.Getter;
 import lombok.Setter;
-
 import javax.persistence.*;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.stream.Collectors;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "address")
 public class Address {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -23,7 +19,6 @@ public class Address {
     private String province;
 
     private String postalCode;
-
 
     @Override
     public String toString() {
